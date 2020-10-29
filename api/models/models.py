f1b18fdba4ec93156a7cd5982d4b93512e2866a0
PaulGOUX27 <paul.goux@wanadoo.fr>
2020-10-29 12:30:25 +0200

    solve issue on model

--- api/models/models.py
+++ api/models/models.py
@@ -1,19 +1,18 @@
 from api.core import Mixin
 from .base import db
 from datetime import datetime
-from sqlalchemy.orm import relationship
-from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
 
-association_table_event_event_category = Table('event_event_category', Base.metadata,
-                                               Column('left_id', Integer, ForeignKey('left.id')),
-                                               Column('right_id', Integer, ForeignKey('right.id'))
-                                               )
+event_event_categories = db.Table('event_event_categories',
+                                  db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True),
+                                  db.Column('event_category_id', db.Integer, db.ForeignKey('event_category.id'),
+                                            primary_key=True)
+                                  )
 
 
 class Event(Mixin, db.Model):
-    """Event Table."""
 
     id = db.Column(db.Integer, primary_key=True)
     start_date = db.Column(db.DateTime)
@@ -21,10 +20,10 @@ class Event(Mixin, db.Model):
     place = db.Column(db.String)
     description = db.Column(db.String)
     title = db.Column(db.String)
-    event_categories = relationship(
+    event_categories = db.relationship(
         "EventCategory",
-        secondary=association_table_event_event_category,
-        back_populates="events")
+        secondary=event_event_categories, lazy='subquery',
+        backref=db.backref('events', lazy=True))
 
     def __init__(self, start_date: datetime, end_date: datetime, place: str, description: str, title: str):
         self.place = place
@@ -38,14 +37,9 @@ class Event(Mixin, db.Model):
 
 
 class EventCategory(Mixin, db.Model):
-    """Event Category Table."""
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    events = relationship(
-        "Event",
-        secondary=association_table_event_event_category,
-        back_populates="event_categories")
 
     def __init__(self, name: str):
         self.name = name
