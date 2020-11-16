1be731bd23b5426a384f607601b8ce4b1e7f191e
PaulGOUX27 <paul.goux@wanadoo.fr>
2020-11-16 15:46:40 +0200

    Fix calendar export

--- api/services/calendarService.py
+++ api/services/calendarService.py
@@ -12,18 +12,18 @@ utc = pytz.utc
 
 def generateCalendar():
     events = eventService.getAll()
-    now = HEL.localize((datetime.now() + timedelta(hours=2)))
+    now = HEL.localize(datetime.now())
     cal = ICalendar()
     cal['version'] = "2.0"
     cal['prodid'] = "//Aalto//CS-E4400//Student Event Manager"
     for event in events:
         iEvent = IEvent()
-        iEvent['uid'] = event.id
+        iEvent['uid'] = str(event.id) + '@aalto-student-event-manager.fi'
         iEvent['Summary'] = event.title
         iEvent['location'] = event.place
         iEvent['Description'] = event.description
-        iEvent['dtstart'] = event.start_date.strftime(fmt)
-        iEvent['dtend'] = event.end_date.strftime(fmt)
+        iEvent['dtstart'] = (event.start_date + timedelta(hours=-2)).strftime(fmt)
+        iEvent['dtend'] = (event.end_date + timedelta(hours=-2)).strftime(fmt)
         iEvent['dtstamp'] = now.strftime(fmt)
         cal.add_component(iEvent)
 
